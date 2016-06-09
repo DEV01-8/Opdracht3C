995939d89948885e1ffb31909a80cc03461591cf
Johan Bos <bosbosjohan@gmail.com>
2016-06-09 12:03:25 +0200

    Cleanup and removed features

--- DEV01-8x3C/src/dev01/pkg8x3c/Main.java
+++ DEV01-8x3C/src/dev01/pkg8x3c/Main.java
@@ -6,11 +6,7 @@
 package dev01.pkg8x3c;
 
 import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Timer;
-import javax.swing.JOptionPane;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import processing.core.PApplet;
@@ -36,12 +32,10 @@ public class Main extends PApplet {
     float raiseWater = 0.100f;
     //Integers
     int frames = 8;
-    int limit = 1000;
     //Logger
     final Logger logger = Logger.getLogger(Main.class);
     //Booleans
     boolean firstRun = true;
-    boolean pause = false;
 
     
     @Override
@@ -51,22 +45,8 @@ public class Main extends PApplet {
         frameRate(1);
         surface.setTitle("Simulatie Overstroming Rotterdam Oost Hoogtebestand 2012");
 
-        results = CSVParser.read(limit);    //Get all items from parseCSV
+        results = CSVParser.read();    //Get all items from parseCSV
         startMap();                         //use map() method to convert RDX and RDY to pixels
-        
-        //Show message about controls
-        JOptionPane.showMessageDialog(frame, "Controls:\n"
-                + "S: Resume \n"
-                + "P: Pause \n"
-                + "R: Reset \n"
-                + "Q: Quit \n"
-                + "1: Slow speed \n"
-                + "2: Medium speed \n"
-                + "3: High speed \n"
-                + "4: Size 500x500 \n"
-                + "5: Size 1000x1000 \n"
-                + "6: Take screenshot"
-        );
     }
 
     @Override
@@ -117,7 +97,7 @@ public class Main extends PApplet {
             firstRun = false;                       //Set firstRun to false to create water
 
         } else if (firstRun == false) {
-            if (pause == false) {
+
                 for (PVector mapping : mappings) {
                     float mapX = mapping.x;
                     float mapY = mapping.y;
@@ -128,29 +108,11 @@ public class Main extends PApplet {
                         fill(color(0, 153, 153));
 
                         ellipse(mapX, mapY, 2f, 2f);            //create ellipse at points of mapped x
-                    }
                 }
             }
         }
     }
 
-    private void reset() {
-        logger.info("Resetting...");
-        firstRun = true;
-        setup();
-    }
-
-    private void quit() {
-        logger.info("Quitting...");
-        JOptionPane.showMessageDialog(frame, "Goodbye!");
-        System.exit(0);
-    }
-    
-    private void takeScreenshot(){
-        Date date = new Date();
-        saveFrame("../Images/screenshot_" + date.getTime() + ".png");
-        logger.info("Screenshot taken @ " + date.getTime());
-    }
 
     @Override
     public void draw() {
@@ -162,100 +124,17 @@ public class Main extends PApplet {
         createMap();
 
         //Increase waterlevel every six frames and display value
-        if (pause == false && frameCount % frames == 0) {
+        if (frameCount % frames == 0) {
             //White rectangle
             fill(255, 255, 255);
-            rect(250, 0, 168, 50);
+            rect(250, 0, 140, 15);
         
             //Increase water level by 0.10
             waterLevel = waterLevel + raiseWater;
 
             //Show value waterLevel
             fill(0, 0, 0);
-            text("Water Level (m): " + df.format(waterLevel), 282, 16);
-        }
-
-        //Show circle green if running and red if paused
-        if (pause == false) {
-            fill(0, 200, 0);
-            ellipse(262, 21, 18, 18);
-        } else {
-            fill(200, 0, 0);
-            ellipse(262, 21, 18, 18);
-        }
-
-        fill(0, 0, 0);
-
-        //Show size
-        if (limit == 500) {
-            text("Size: 500x500", 282, 45);
-        } else {
-            text("Size: 1000x1000", 282, 45);
-        }
-
-        //Show current speed
-        fill(0, 0, 0);
-        switch (frames) {
-            case 8:
-                text("Speed: Slow", 282, 30);
-                break;
-            case 4:
-                text("Speed: Medium", 282, 30);
-                break;
-            case 2:
-                text("Speed: High", 282, 30);
-                break;
-            default:
-                text("Speed: Slow", 282, 30);
-                break;
-        }
-    }
-
-    @Override
-    //Controls
-    public void keyPressed() {
-        switch (key) {
-            case 's':                       //Start, Resume
-                pause = false;
-                raiseWater = 0.100f;
-                logger.info("Resuming...");
-                break;
-            case 'p':                       //Pause
-                pause = true;
-                raiseWater = 0.0f;
-                logger.info("Paused...");
-                break;
-            case 'r':                       //Reset
-                reset();
-                break;
-            case 'q':                       //Quit
-                quit();
-                break;
-            case '1':                       //Speed 1: update every 6 frames
-                frames = 8;
-                break;
-            case '2':                       //Speed 2: update every 4 frames
-                frames = 4;
-                break;
-            case '3':                       //Speed 3: update every 2 frames
-                frames = 2;
-                break;
-            case '4':                       //500x500
-                limit = 500;
-                reset();
-                break;
-            case '5':                       //1000x1000
-                limit = 1000;
-                reset();
-                break;
-            case '6':                       //Save screenshot
-                takeScreenshot();
-                break;
-            default:
-                frames = 6;
-                pause = false;
-                limit = 1000;
-                break;
+            text("Water Level (m): " + df.format(waterLevel), 254, 16);
         }
     }
 }
